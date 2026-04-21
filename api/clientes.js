d9e75c6f2db88e2c9ce15b103d09d91fcf69c8b9
MarcoRMarques <161774713+MarcoRMarques@users.noreply.github.com>
2026-04-21 15:20:13 -0300

    Add files via upload

--- api/clientes.js
+++ api/clientes.js
@@ -29,7 +29,10 @@ export default async function handler(req, res) {
      ========================================================= */
   res.setHeader("Access-Control-Allow-Origin", "*");
   /* 🔹 LIBERA TODOS OS MÉTODOS NECESSÁRIOS */
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS",
+  );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   /* 🔹 Responde requisição preflight (CORS) */
